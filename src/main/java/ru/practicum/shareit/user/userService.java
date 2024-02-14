e705c4fc9d3fc427524ea8415cfc43679a3fc0ea
Toropov Alex <toropov_alex@mail.ru>
2024-02-14 23:27:12 +0200

    refactor: пройдены в Постмен все тесты, связанные с user

--- src/main/java/ru/practicum/shareit/user/userService.java
+++ src/main/java/ru/practicum/shareit/user/userService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 interface UserService {
     List<User> getAllUsers();
+    User getUser(Long userId);
     User saveUser(User user);
     User updateUser(User user, Long userId);
+    void deleteUser(Long userId);
 }
