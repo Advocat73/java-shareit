e45e1df67403a35590a5fcf8f6c971bfb8a2db96
Toropov Alex <toropov_alex@mail.ru>
2024-02-15 21:29:54 +0200

    ТЗ 13: первый выпуск: устранен недостаток компиляции

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.user;
 
+import ru.practicum.shareit.user.dto.UserDto;
+
 import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
     User getUser(Long userId);
     User saveUser(User user);
-    User updateUser(User user, Long userId);
+    User updateUser(UserDto user, Long userId);
     void deleteUser(Long userId);
 }
