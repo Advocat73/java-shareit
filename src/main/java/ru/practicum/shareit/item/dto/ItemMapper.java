d2fee1839d19a7bed95de13a1b944150a775936b
Toropov Alex <toropov_alex@mail.ru>
2024-03-12 01:45:25 +0200

    ФЗ-4: первый выпуск

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -7,7 +7,7 @@ import ru.practicum.shareit.user.User;
 @NoArgsConstructor
 public class ItemMapper {
     public static ItemDto toItemDto(Item item) {
-        return (item == null) ? null : _toItemDto(new ItemDto(), item);
+        return (item == null) ? null : toItemDto(new ItemDto(), item);
     }
 
     public static Item fromItemDto(ItemDto itemDto, User owner) {
@@ -23,7 +23,7 @@ public class ItemMapper {
     }
 
 
-    protected static ItemDto _toItemDto(ItemDto itemDto, Item item) {
+    protected static ItemDto toItemDto(ItemDto itemDto, Item item) {
         itemDto.setId(item.getId());
         itemDto.setName(item.getName());
         itemDto.setDescription(item.getDescription());
--- src/main/java/ru/practicum/shareit/item/dto/ItemWithDatesBookingMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemWithDatesBookingMapper.java
@@ -4,6 +4,6 @@ import ru.practicum.shareit.item.Item;
 
 public class ItemWithDatesBookingMapper {
     public static ItemWithDatesBookingDto toItemWithDatesBookingDto(Item item) {
-        return (item == null) ? null : (ItemWithDatesBookingDto) ItemMapper._toItemDto(new ItemWithDatesBookingDto(), item);
+        return (item == null) ? null : (ItemWithDatesBookingDto) ItemMapper.toItemDto(new ItemWithDatesBookingDto(), item);
     }
 }
