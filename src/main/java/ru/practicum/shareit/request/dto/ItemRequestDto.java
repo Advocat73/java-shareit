c2a7ea7712f63f1174d88570d103139eed54d7e5
Toropov Alex <toropov_alex@mail.ru>
2024-03-21 21:57:59 +0200

    ФЗ-15: убраны стайл ошибки, перед тестами

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,16 +1,11 @@
 package ru.practicum.shareit.request.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.practicum.shareit.user.User;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
-//@AllArgsConstructor
 public class ItemRequestDto {
     Long id;
     @NotBlank(message = "Должно быть описание запроса")
