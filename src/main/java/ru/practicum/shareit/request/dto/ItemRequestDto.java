a021822f747cd31bb89d69448cda2cb26dcc580b
Toropov Alex <toropov_alex@mail.ru>
2024-03-25 20:35:08 +0200

    ФЗ-15: Первый релиз, добавлен образец javadoc

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -8,7 +8,13 @@ import java.time.LocalDateTime;
 @Data
 public class ItemRequestDto {
     Long id;
+    /**
+     текст запроса, содержащий описание требуемой вещи
+     */
     @NotBlank(message = "Должно быть описание запроса")
     private String description;
+    /**
+     дата и время создания запроса
+     */
     private LocalDateTime created = LocalDateTime.now();
 }
